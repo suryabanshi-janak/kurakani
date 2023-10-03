6342c5d444f10e6f9794d55f54b04f2f5f139fd4
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-10-03 11:53:22 +0545

    feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,14 +28,14 @@ export default function Home() {
           target='_blank'
           className={buttonVariants({
             size: 'lg',
-            className: 'mt-5 text-base',
+            className: 'mt-5 md:text-lg',
           })}
         >
           Get started <ArrowRight className='ml-2 w-5 h-5' />
         </Link>
       </MaxWidthWrapper>
 
-      {/* Product preview */}
+      {/* Product Preview */}
       <div>
         <div className='relative isolate'>
           <div
@@ -79,6 +79,83 @@ export default function Home() {
               }}
               className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#fef08a] to-[#4ade80] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
             />
+          </div>
+        </div>
+      </div>
+
+      {/* Feature Section */}
+      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div>
+            <div className='mx-auto max-w-2xl sm:text-center'>
+              <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+                Start chatting in minutes
+              </h2>
+              <p className='mt-4 text-lg text-gray-600'>
+                Chatting to your PDF files has never been easier than with
+                Kurakani.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-600'>Step 1</span>
+              <span className='text-xl font-semibold'>
+                Sign up for an account
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Either starting out with a free plan or choose our{' '}
+                <Link
+                  href='/pricing'
+                  className='text-green-700 underline underline-offset-2'
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-600'>Step 2</span>
+              <span className='text-xl font-semibold'>
+                Upload your PDF file
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                We&apos;ll process your file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-600'>Step 3</span>
+              <span className='text-xl font-semibold'>
+                Start asking questions
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                It&apos;s that simple. Try out Kurakani today - it really takes
+                less than a minute.
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className='mx-auto max-w-5xl px-6 lg:px-8'>
+          <div className='mt-16 sm:mt-24 flow-root'>
+            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <Image
+                src='/file-upload-preview.jpg'
+                alt='fileupload preview'
+                width={1419}
+                height={732}
+                quality={100}
+                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              />
+            </div>
           </div>
         </div>
       </div>
