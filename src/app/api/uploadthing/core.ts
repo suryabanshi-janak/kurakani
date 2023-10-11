7f6ae129b8c8ba5170188a9dc4b869bd5cb98d5e
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-10-11 22:38:56 +0545

    pinecone store

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,11 @@
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
 import { getAuthSession } from '@/lib/auth-option';
 import { db } from '@/lib/db';
-import { createUploadthing, type FileRouter } from 'uploadthing/next';
+
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { getPineconeClient } from '@/lib/pinecone';
 
 const f = createUploadthing();
 
@@ -23,6 +28,55 @@ export const ourFileRouter = {
           uploadStatus: 'PENDING',
         },
       });
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+
+        const blob = await response.blob();
+
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+
+        // vectorize and index entire document
+        const pinecone = await getPineconeClient();
+        const pineconeIndex = pinecone.Index('kurakani');
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        const vectorStore = await PineconeStore.fromDocuments(
+          pageLevelDocs,
+          embeddings,
+          {
+            pineconeIndex,
+            // namespace: createdFile.id,
+          }
+        );
+
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
